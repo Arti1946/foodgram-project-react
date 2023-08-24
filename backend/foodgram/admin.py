25465165efaf62a2470e7aacbb9e6c21c10aa8b8
Артур Галиаскаров <artigaliaskarov@yandex.ru>
2023-08-24 05:28:42 +0300

    all Done

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Favorite, Ingredients, Recipes, Tags
+from .models import Ingredients, Recipes, Tags
 
 
 class IngredientAdmin(admin.ModelAdmin):
